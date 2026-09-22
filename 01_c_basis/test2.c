c1c5d3bf8cbf13d1b48c23eb332e3a2f2a6d42af
jtxxy0107 <18504801687@163.com>
2026-09-22 23:03:12 +0800

    BMI计算

---
+++ 01_c_basis/test2.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+int main()
+{   double a = 182.0;
+    double b = 78.5;
+    double c = 10000*b/(a*a);
+    printf("你的身高是：%f厘米\n",a );
+    printf("你的体重是：%f千克\n",b);
+    printf("你的BMI是：%f\n",c);
+
+
+    return 0;
+}
